5a35150fc0150af90c97ca000feb368c62e63588
Rana Emad <rana3emad@gmail.com>
2020-03-19 06:01:48 +0000

    initial commit

---
+++ src/ArtisanUiServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace REM\ArtisanUi;
+
+use Illuminate\Support\ServiceProvider;
+
+class ArtisanUiServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+    }
+}
