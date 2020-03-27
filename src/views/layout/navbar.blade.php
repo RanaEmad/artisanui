efb7b78a0c01392e5d737ecf950a87a29c4af513
Rana Emad <rana3emad@gmail.com>
2020-03-27 12:47:11 +0000

    add navbar

--- src/views/layout/navbar.blade.php
+++ src/views/layout/navbar.blade.php
@@ -1,14 +1,43 @@
-<div class="container-xlg mb-5">
+<div class="container-xlg">
     <nav class="navbar navbar-expand-lg navbar-light bg-dark">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse text-light" id="navbarNav">
+        <div class="collapse navbar-collapse text-light justify-content-center" id="navbarNav">
           <ul class="navbar-nav">
             <li class="nav-item">
-              <a class="nav-link text-light" href="#">Model</a>
+              <a class="navbar-brand text-light">Artisan UI</a>
             </li>
           </ul>
         </div>
       </nav>
+</div>
+<div class="container-xlg mb-5">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse  justify-content-center" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link text-info h6" href="/artisanui/models">Model</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-info h6" href="/artisanui/controllers">Controller</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-info h6" href="/artisanui/migrations">Migration</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-info h6" href="/artisanui/factories">Factory</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-info h6" href="/artisanui/seeders">Seeder</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-info h6" href="/artisanui/tests">Test</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
 </div>
